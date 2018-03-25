d85620b9ffe149d64882b8b8a460168357559832
XuzSu <XuzSu@192.168.0.16>
2018-03-25 17:44:24 +0800

    改善spawn 寫法

--- 71Ess/src/aa/plugin/system/Spawn.java
+++ 71Ess/src/aa/plugin/system/Spawn.java
@@ -1,7 +1,5 @@
 package aa.plugin.system;
 
-import java.io.File;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
@@ -9,7 +7,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -79,15 +76,14 @@ public class Spawn implements CommandExecutor, Listener
 	{
 		Player p = e.getPlayer();
 		
-		File file = new File("plugins/71ess/spawn.yml");
-		YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+		FileConfiguration spawn = Main.plugin.sc;
 		
-		World w = Bukkit.getWorld(cfg.getString("world"));
-		double x = cfg.getDouble("spawnX");
-		double y = cfg.getDouble("spawnY");
-		double z = cfg.getDouble("spawnZ");
-		float pitch = (float) cfg.getDouble("spawnPitch");
-		float yaw = (float) cfg.getDouble("spawnYaw");
+		World w = Bukkit.getWorld(spawn.getString("world"));
+		double x = spawn.getDouble("spawnX");
+		double y = spawn.getDouble("spawnY");
+		double z = spawn.getDouble("spawnZ");
+		float pitch = (float) spawn.getDouble("spawnPitch");
+		float yaw = (float) spawn.getDouble("spawnYaw");
 		
 		Location loc = new Location(w, x, y, z, pitch, yaw);
 		p.teleport(loc);
