dd229d03aea5423633df9af3f194c0abb6dbf59f
XuzSu <XuzSu@192.168.0.16>
2018-03-27 21:28:03 +0800

    還沒完成 我卡關 你等等打/tp 看看會發生什麼 多用幾次

--- 71Ess/src/aa/plugin/system/Teleport.java
+++ 71Ess/src/aa/plugin/system/Teleport.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -18,41 +17,59 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
+import aa.plugin.function.createItem;
 import aa.plugin.main.MessageManager;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 
 public class Teleport implements CommandExecutor, Listener
-{
-	List<Inventory> inventories = new ArrayList<>();
+{	
+	int listMaximum = 45;
+	int inventorySizeLevel = 6;
+	ItemStack[] items = new ItemStack[inventorySizeLevel * 9];
+	List<Player> playersList = new ArrayList<Player>();
+	
+	Inventory inv = Bukkit.createInventory(null, inventorySizeLevel * 9, "線上玩家列表 (傳送用)");
+	int page;
+	
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String lable, String[] args)
 	{
+		
 		if (sender instanceof Player)
 		{
 			Player p = (Player) sender;
-			if (sender.hasPermission("71ess.gamemode"))
+			if (sender.hasPermission("71ess.teleport"))
 			{
 				if (args.length == 0)
-				{
-					Inventory inv = Bukkit.createInventory(null, 36, "在線玩家列表 (傳送用)");
-					p.playSound(p.getLocation(), Sound.BLOCK_NOTE_PLING, 4.0F, 4.0F);
+				{	
 					
-					for (Player OnlinePlayers : Bukkit.getOnlinePlayers())
-					{	
-						if (OnlinePlayers != p.getPlayer())
+					playersList.addAll(Bukkit.getOnlinePlayers());
+					
+					for (int site = page * listMaximum; site < playersList.size() && site < ((page + 1) * listMaximum); site++)
+					{
+						//if (playersList.get(site) != p)
 						{
 							ItemStack item = new ItemStack(Material.SKULL_ITEM, 1, (short)3);
-							SkullMeta meta = (SkullMeta)item.getItemMeta();
-							meta.setDisplayName(OnlinePlayers.getName());
-							meta.setOwningPlayer(OnlinePlayers);
+							SkullMeta meta = (SkullMeta) item.getItemMeta();
+							meta.setDisplayName("§e" + playersList.get(site).getName());
+							meta.setOwningPlayer(playersList.get(site));
 							item.setItemMeta(meta);
-							
-							inv.addItem(item);
-							
+								
+						items[site - (page * listMaximum)] = item;
+						playersList.remove(site);
 						}
 					}
+					if (page > 0)
+					{
+						items[51] = createItem.createItem(Material.FEATHER, 0, "上一頁", " ");
+					}
+					if (playersList.size() - (listMaximum * page + 1) > 0)
+					{
+						items[52] = createItem.createItem(Material.FEATHER, 0, "下一頁", " ");
+					}
+					inv.setStorageContents(items);
 					p.openInventory(inv);
 				}
 			}
@@ -64,26 +81,23 @@ public class Teleport implements CommandExecutor, Listener
 	@EventHandler
 	public void onInventoryClick(InventoryClickEvent e)
 	{
-		if (e.getInventory().getName().contains("在線玩家列表 (傳送用)"))
+		if (e.getInventory().getName().contains("線上玩家列表 (傳送用)"))
 		{
 			Player p = (Player) e.getWhoClicked();
 			e.setCancelled(true);
 				
 			if (e.getCurrentItem() == null || e.getCurrentItem().getType() == Material.AIR) return;
 			
-			for (Player OnlinePlayers : Bukkit.getOnlinePlayers())
+			for (int site = page * listMaximum; site < playersList.size() && site < (page + 1 * listMaximum); site++)
 			{
-				if (OnlinePlayers != p.getPlayer())
+				if (e.getCurrentItem().getItemMeta() instanceof SkullMeta)
 				{
-					if (e.getCurrentItem().getItemMeta().getDisplayName().equals(OnlinePlayers.getName()))
-					{
-						Location loc = OnlinePlayers.getLocation();
-						p.teleport(loc);
-						
-						p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(MessageManager.TELEPORT_TOPLAYER));
-					}
+					Location loc = playersList.get(site).getLocation();
+					p.teleport(loc);
+					p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(MessageManager.TELEPORT_TOPLAYER));
 				}
-			}	
+			}
+
 		}
 	}
 }
