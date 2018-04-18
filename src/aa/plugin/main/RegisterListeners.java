2fbd90bdb14f5feea4234da854627dda691c9d0a
XuzSu <XuzSu@192.168.0.16>
2018-04-18 13:41:00 +0800

    新增交易頻道

--- src/aa/plugin/main/RegisterListeners.java
+++ src/aa/plugin/main/RegisterListeners.java
@@ -8,6 +8,7 @@ import aa.plugin.system.GameModes;
 import aa.plugin.system.God;
 import aa.plugin.system.SignEditor;
 import aa.plugin.system.Spawn;
+import aa.plugin.system.TradeChannel;
 import aa.plugin.system.Weather;
 
 public class RegisterListeners
@@ -24,6 +25,7 @@ public class RegisterListeners
 		plugin.getServer().getPluginManager().registerEvents(new Spawn(), plugin);
 		plugin.getServer().getPluginManager().registerEvents(new God(), plugin);
 		plugin.getServer().getPluginManager().registerEvents(new SignEditor(), plugin);
+		plugin.getServer().getPluginManager().registerEvents(new TradeChannel(), plugin);
 		
 		//GUIs
 		plugin.getServer().getPluginManager().registerEvents(new MainGUI(), plugin);
---
+++ src/aa/plugin/system/TradeChannel.java
@@ -0,0 +1,32 @@
+package aa.plugin.system;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import aa.plugin.function.cooldown;
+
+public class TradeChannel implements Listener
+{
+	@EventHandler    
+	public void tradeChannel(AsyncPlayerChatEvent e)
+	{
+		Player p = e.getPlayer();
+		if (!cooldown.CooldownCheck(p.getName() + "交易頻道"))
+		{
+			p.sendMessage("§f§l您的交易頻道發言正於冷卻當中 (冷卻時間為 5 秒鐘一次)");
+			e.setCancelled(true);
+			return;
+		}
+			if (e.getMessage().startsWith("$"))
+			{
+				String msg = e.getMessage().substring(1);
+				p.getServer().broadcastMessage("");
+				p.getServer().broadcastMessage("§6§l交易頻道 §7§l>> [§a" + p.getDisplayName() + "§7§l] §f" + msg);
+				p.getServer().broadcastMessage("");
+				e.setCancelled(true);
+				cooldown.CooldownSet(p.getName() + "_交易頻道", 5);
+			}
+	}
+}
