05f15e867e5baf676ef141d46826606e618b23d7
Sigtuna <Sigtunatw@gmail.com>
2018-04-24 16:57:10 +0800

    補上SignEditorBySigtuna.java

---
+++ src/aa/plugin/system/SignEditorBySigtuna.java
@@ -0,0 +1,221 @@
+package aa.plugin.system;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.Location;
+import org.bukkit.block.Sign;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+class SignData{
+	
+	Player creator;
+	String[] info;
+	
+}
+
+class SignSet{
+	
+	Player creator;
+	SignData sd;
+	Location main_location;
+	List<Location> list = new ArrayList<Location>();
+	List<Location> unclick = new ArrayList<Location>();
+	
+}
+
+public class SignEditorBySigtuna implements Listener,CommandExecutor{
+
+	static Map<Player,SignSet> map = new HashMap<Player,SignSet>();
+	
+	@EventHandler
+	public void onClick(PlayerInteractEvent e) {
+		
+		//Action
+		if(e.getAction() != Action.LEFT_CLICK_BLOCK || !e.getPlayer().isSneaking()) {
+			
+			return;
+			
+		}
+		
+		e.setCancelled(true);
+		
+		//Check click block is a sign.
+		if(!(e.getClickedBlock().getState() instanceof Sign)) {
+			
+			return;
+			
+		}
+		
+		Sign sign = (Sign)e.getClickedBlock().getState();
+		Player player = e.getPlayer();
+		
+		if(map.get(player) == null) {
+			
+			SignSet set = new SignSet();	
+			SignData sd = new SignData();
+			
+			sd.creator = player;
+			sd.info = sign.getLines();
+			
+			set.sd = sd;
+			set.main_location = e.getClickedBlock().getLocation();
+			
+			map.put(player,set);
+			
+			player.sendMessage("新增主告示牌");
+			
+		} else {
+			
+			Location loc = e.getClickedBlock().getLocation();
+			
+			SignSet set = map.get(player);
+			
+			if(set.unclick.contains(loc)) {
+				
+				set.unclick.remove(loc);
+				
+				set.list.remove(loc);
+				
+				player.sendMessage("反向選擇告示牌");
+				
+			}else {
+				
+				if(set.main_location.equals(loc)) {
+					
+					player.sendMessage("與主告示牌座標重疊，無意義行為。");
+					
+				}
+				
+				SignData sd = new SignData();
+				
+				sd.creator = player;
+				sd.info = sign.getLines();
+				
+				set.unclick.add(loc);
+				
+				set.list.add(loc);
+				
+				player.sendMessage("新增告示牌" + set.list.size());
+				
+			}
+			
+			map.put(player, set);
+			
+		}
+		
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		if(!(sender instanceof Player)) {
+			
+			sender.sendMessage("你不是玩家，無法使用這個功能");
+			return false;
+			
+		}
+		
+		Player player = (Player) sender;
+		
+		if(!player.hasPermission("71ess.signedit")) {
+			
+			sender.sendMessage("不讓你用");
+			return false;
+			
+		}
+		
+		if(args.length == 0) {
+			
+			help(player);
+			
+		}
+		
+		switch(args[0]) {
+		
+		case "copy":
+			
+			copy(player);
+			break;
+			
+		case "clear":
+			
+			map.remove(player);
+			player.sendMessage("清除成功。");
+			break;
+			
+		default:
+			
+			help(player);
+			break;
+			
+		}
+		
+		return false;
+	}
+	
+	public void help(Player player) {
+		
+		player.sendMessage("=================================================================");
+		player.sendMessage("");
+		player.sendMessage("/signedit <下方指令>");
+		player.sendMessage("- copy" + "     " + "將主告示牌訊息複製到 所有記錄的告示牌上");
+		player.sendMessage("- clear" + "    " + "清除所有儲存的告示牌記錄(包括主告示牌)");
+		player.sendMessage("");
+		player.sendMessage("=================================================================");
+		
+	}
+	
+	public void copy(Player player) {
+		
+		if(map.get(player) == null) {
+			
+			player.sendMessage("沒有儲存任何告示牌。");
+			return;
+			
+		}
+		
+		SignSet set = map.get(player);
+		
+		List<Location> data = set.list;
+		SignData sd = set.sd;
+		
+		for(int i = 0; i < data.size(); i++) {
+			
+			Location loc = (Location) data.get(i);
+			
+			if(!(loc.getBlock().getState() instanceof Sign)) {
+				
+				player.sendMessage("在 " + loc.getBlockX() + " , " + loc.getBlockY() + " , " + loc.getBlockZ() + " 的告示牌已被拆除，已忽略。");
+				continue;
+				
+			}
+			
+			Sign sign = (Sign) loc.getBlock().getState();
+			
+			for(int j = 0; j < sd.info.length; j++) {
+				
+				sign.setLine(j, sd.info[j]);
+				
+			}
+			
+			sign.update();
+			
+		}
+		
+		map.remove(player);
+		
+		player.sendMessage("設置完成。");
+		
+	}
+
+}
