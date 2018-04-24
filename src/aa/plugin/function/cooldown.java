3fe73f097014391304c411df9e2371a916438351
XuzSu <XuzSu@192.168.0.16>
2018-04-24 23:40:43 +0800

    移除GlobalVar在cooldown加入map

--- src/aa/plugin/function/cooldown.java
+++ src/aa/plugin/function/cooldown.java
@@ -1,20 +1,20 @@
 package aa.plugin.function;
 
-
-
-import aa.plugin.main.GlobalVar;
+import java.util.HashMap;
 
 public class cooldown 
 {
+	static HashMap<String, Long> CooldownMap = new HashMap<String, Long>();
+	
 	 // 冷卻檢查
     public static Boolean CooldownCheck(String CooldownName) {
         // 判斷是否存在冷卻
-        if (GlobalVar.CooldownMap.containsKey(CooldownName)) {
+        if (CooldownMap.containsKey(CooldownName)) {
             // 判斷冷卻時間是否已過
-            if (GlobalVar.CooldownMap.get(CooldownName) > System.currentTimeMillis()) {
+            if (CooldownMap.get(CooldownName) > System.currentTimeMillis()) {
                 return false;
             } else {
-                GlobalVar.CooldownMap.remove(CooldownName);
+                CooldownMap.remove(CooldownName);
             }
         }
         return true;
@@ -23,20 +23,20 @@ public class cooldown
     // 冷卻設定
     public static void CooldownSet(String CooldownName, Integer Sec) {
         // 判斷是否存在冷卻
-        if (GlobalVar.CooldownMap.containsKey(CooldownName)) {
-            GlobalVar.CooldownMap.replace(CooldownName, System.currentTimeMillis() + (Sec * 1000));
+        if (CooldownMap.containsKey(CooldownName)) {
+            CooldownMap.replace(CooldownName, System.currentTimeMillis() + (Sec * 1000));
         } else {
-            GlobalVar.CooldownMap.put(CooldownName, System.currentTimeMillis() + (Sec * 1000));
+            CooldownMap.put(CooldownName, System.currentTimeMillis() + (Sec * 1000));
         }
     }
 
     // 冷卻設定 
     public static void CooldownSetByMillisecond(String CooldownName, Integer Millisecond) {
         // 判斷是否存在冷卻
-        if (GlobalVar.CooldownMap.containsKey(CooldownName)) {
-            GlobalVar.CooldownMap.replace(CooldownName, System.currentTimeMillis() + Millisecond);
+        if (CooldownMap.containsKey(CooldownName)) {
+            CooldownMap.replace(CooldownName, System.currentTimeMillis() + Millisecond);
         } else {
-            GlobalVar.CooldownMap.put(CooldownName, System.currentTimeMillis() + Millisecond);
+            CooldownMap.put(CooldownName, System.currentTimeMillis() + Millisecond);
         }
     }
 }
